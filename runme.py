fcb30c4094ce9cfff2012d28d4a699ea0bf6271c
Liam Mc Cormick <ljmc2000@gmail.com>
2018-08-02 00:35:49 +0100

    added the moon then promptly left it behind

--- runme.py
+++ runme.py
@@ -3,21 +3,25 @@ from planet import *
 
 width,height=500,500
 centre=int(width/2),int(height/2)
-frames=999
+frames=500
 
 sun=Circle("yellow",50,centre)
 earth=Planet("blue",150,30,sun,(centre[0],centre[1]-170))
 earth.addIsland(FreeShape("green",(10,10),(-30,0),(10,-30),dpath="M %CENTRE% m %POINT% l %POINT% l %POINT%"))
+ex,ey=earth.getCentre()
+moon=Planet("grey",27,5,earth,(ex,ey-50))
 
 for i in range(0,frames):
-	filename="file%4d.svg" % i
+	filename="file%3d.svg" % i
 	filename=filename.replace(" ","0")
 	out=open(filename,"w+")	
 
 	out.write("<svg width=\"%s\" height=\"%s\">" % (width,height))
 	out.write(sun.__str__())
 	out.write(earth.__str__())
+	out.write(moon.__str__())
 	out.write("</svg>")
 	out.close()
 
 	earth.sunOrbit()
+	moon.sunOrbit()
