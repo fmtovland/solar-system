a2e2eaae7da87f7d3974be053b6baceb300c3002
Liam Mc Cormick <ljmc2000@gmail.com>
2018-08-02 13:24:59 +0100

    tried to fix the moon turning a different speed to it's orbit

--- runme.py
+++ runme.py
@@ -4,17 +4,17 @@ from planet import *
 #width,height=3840,2160
 width,height=500,500
 centre=int(width/2),int(height/2)
-frames=500
+frames=365*24
 
 sun=Sun("yellow",50,centre)
 earth=Planet("blue",365,24,30,sun,(centre[0],centre[1]-170))
 earth.addIsland(FreeShape("green",(10,10),(-30,0),(10,-30),dpath="M %CENTRE% m %POINT% l %POINT% l %POINT%"))
 ex,ey=earth.getCentre()
-moon=Planet("grey",27,27,5,earth,(ex,ey-50))
-#moon.addIsland(FreeShape("red",(2.5,2.5),(-5,0),dpath="M %CENTRE% l %POINT% l %POINT%"))
+moon=Planet("grey",1,27,5,earth,(ex,ey-50))
+moon.addIsland(FreeShape("red",(2.5,2.5),(-5,0),dpath="M %CENTRE% l %POINT% l %POINT%"))
 
 for i in range(0,frames):
-	filename="/tmp/test/file%3d.svg" % i
+	filename="/tmp/test/file%5d.svg" % i
 	filename=filename.replace(" ","0")
 	out=open(filename,"w+")
 
